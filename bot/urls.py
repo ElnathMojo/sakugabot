eecfc0cd9b573c8a842c767d555b32100c417849
ElnathMojo <elnathmojo@gmail.com>
2018-08-06 00:31:52 +0800

    admin link

--- bot/urls.py
+++ bot/urls.py
@@ -1,6 +1,8 @@
 from bot import views
 from django.urls import path
 
+app_name = 'bot'
+
 urlpatterns = [
     path('log/', views.ShowLog.as_view(), name='log'),
     path('test/', views.test, name='test')
